85c335aff09617d71f35012f9a602571d0600925
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-11-04 23:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+bands.sort()
+
+let unorderList = document.getElementById("unorderList")
+bands.forEach((item, index) => {
+	let listItem = document.createElement("li")
+	listItem.classList.add("item")
+	listItem.innerHTML = item
+	unorderList.append(listItem)
+})
